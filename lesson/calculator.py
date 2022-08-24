e527fb58a3329888134f3eb51b764e1ee7bdfada
o.nikolaev <o.nikolaev@rtinform.ru>
2022-08-24 09:01:20 +0300

    Initial commit

---
+++ lesson/calculator.py
@@ -0,0 +1,22 @@
+def calculator(expression):
+    allowed = '+-/*'
+    if not any(sign in expression for sign in allowed):
+        raise ValueError(f'Выражение должно содержать хотя бы один арифметический знак ({allowed})')
+
+    for sign in allowed:
+        if sign in expression:
+            try:
+                left, right = expression.split(sign)
+                left, right = int(left), int(right)
+                return {
+                    '+': lambda a, b: a + b,
+                    '-': lambda a, b: a - b,
+                    '*': lambda a, b: a * b,
+                    '/': lambda a, b: a / b
+                }[sign](left, right)
+            except (ValueError, TypeError):
+                raise ValueError('Выражение содержать 2 целых числа и один знак арифметической операции')
+
+
+if __name__ == '__main__':
+    print(calculator('10/5'))
---
+++ tests/calculator_test.py
@@ -0,0 +1,45 @@
+from unittest import TestCase, main
+from lesson.calculator import calculator
+
+
+class CalculatorTest(TestCase):
+    def test_plus(self):
+        self.assertEqual(calculator('2+2'), 4)
+
+    def test_minus(self):
+        self.assertEqual(calculator('3-1'), 2)
+
+    def test_multiple(self):
+        self.assertEqual(calculator('4*4'), 16)
+
+    def test_divide(self):
+        self.assertEqual(calculator('10/5'), 2.0)
+
+    def test_no_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('glavryba')
+        self.assertEqual('Выражение должно содержать хотя бы один арифметический знак (+-/*)', e.exception.args[0])
+
+    def test_two_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2+2+2')
+        self.assertEqual('Выражение содержать 2 целых числа и один знак арифметической операции', e.exception.args[0])
+
+    def test_many_signs(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2+3*10/5')
+        self.assertEqual('Выражение содержать 2 целых числа и один знак арифметической операции', e.exception.args[0])
+
+    def test_no_ints(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('2.2+3.0')
+        self.assertEqual('Выражение содержать 2 целых числа и один знак арифметической операции', e.exception.args[0])
+
+    def test_strings(self):
+        with self.assertRaises(ValueError) as e:
+            calculator('a+b')
+        self.assertEqual('Выражение содержать 2 целых числа и один знак арифметической операции', e.exception.args[0])
+
+
+if __name__ == '__main__':
+    main()
